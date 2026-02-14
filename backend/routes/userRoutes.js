c4bf712515563d32c224fb71d4f9681300a794d3
seunayolu <seunayolu@gmail.com>
2026-02-14 10:40:44 +0100

    School app amplify frontend

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController'); // Change to non-destructuring import
-
-router.post('/register', userController.registerUser);
-
-module.exports = router;
